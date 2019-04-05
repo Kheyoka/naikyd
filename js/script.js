f15bef2694446719c458df7bfc2f0fb8ec7fc42c
Kseniya G Vo <vwk8888@gmail.com>
2019-04-05 20:24:22 +0300

    prototype

---
+++ js/script.js
@@ -0,0 +1,7 @@
+setInterval(function() {
+    $('.chicken_f2_static').toggleClass('off');
+    $('.chicken_f2_glow').toggleClass('off');
+    $('.chicken_f1_static').toggleClass('off');
+    $('.chicken_f1_glow').toggleClass('off');
+    
+  }, 2000);
